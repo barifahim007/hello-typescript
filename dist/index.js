e7ad0cad2c116f746250167bd6efac87ca6313dc
barifahim007 <ehsanulbarifahim7@gmail.com>
2023-12-06 19:00:29 +0600

    setup typescript and nvm

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const course = "next level-2 bal diya a headech development";
+console.log(course);
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+const course: string = "next level-2 bal diya a headech development";
+console.log(course);
